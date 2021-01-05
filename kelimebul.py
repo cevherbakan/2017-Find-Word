2a80c778dd7242694ec8b4f466a3c3a76cb1c032
cevherbakan <53389814+cevherbakan@users.noreply.github.com>
2021-01-05 12:10:12 +0300

    Update kelimebul.py

--- kelimebul.py
+++ kelimebul.py
@@ -1,13 +1,11 @@
 import os
 
 
-global adres
-adres="\Desktop\listeleme deneme"
-
 
 
 def uzanti_kaydet(uzanti):
     global liste
+    print "uzanti kaydet"
 
  
     ths = open("uzantidosyasi.uzanti", "w")
@@ -19,26 +17,21 @@ def uzanti_kaydet(uzanti):
         
 
     except WindowsError:
-        
-        yyy_metin=''
-        yeni_metin=uzanti.split(chr(92))
-        yeni_metin[-1]=''
-        
-        for i in yeni_metin:
-            yyy_metin=yyy_metin+i+chr(92)
-
-        print "uzanti",yyy_metin
-
-        uzanti_kaydet(yyy_metin)
 
 
+        return False
+    
+    return True
 
     
 
 
-def dosya_kontrol():
+def dosya_kontrol(kelime):
+    klasor_listesi=''
 
     for dosya in liste:
+        kilit=False
+        
         for harf in dosya:
             
             if harf==".":
@@ -46,8 +39,16 @@ def dosya_kontrol():
                 metin = dosya_klasor.read()
                 dosya_klasor.close()
                 
-                arama(metin+chr(92)+dosya,"merh")
+                arama(metin+chr(92)+dosya,kelime)
+                kilit= False
                 break
+            else:
+                kilit= True
+        if kilit==True:    
+            klasor_listesi+=dosya+';'
+    print klasor_listesi
+        
+    return klasor_listesi
     
 
 def arama(dosya_adi,kelime):
@@ -61,7 +62,7 @@ def arama(dosya_adi,kelime):
         if metin[i]==kelime[sayac]:
             sayac+=1
             if sayac==len(kelime)-1:
-                print "Dosya adi=",dosya_adi,"  No=",i,"  bulundu"
+                print "Dosya adresi=",dosya_adi,"  No=",i,"  ****bulundu\n"
                 break
         else:
             sayac=0
@@ -72,42 +73,61 @@ def arama(dosya_adi,kelime):
 
     
 
-def klasor_kontrol():
-    dosya_kontrol()
+def klasor_kontrol(kelime):
     
-    for dosya in liste:
-        anahtar=False
-        for harf in dosya:
-            
-            if harf==".":
-                anahtar=False
+    klasor_liste=dosya_kontrol(kelime)
+    yeni_klas_liste=klasor_liste.split(';')
+    print yeni_klas_liste
 
-                break
-            elif 0<len(dosya):
-                anahtar=True
-                
-        if(anahtar==True):
-            print "dosya",dosya
+
+    if (len(yeni_klas_liste)!=1 and yeni_klas_liste[0]!='':
+        for dosya in yeni_klas_liste:
+            print dosya
+            
             dosya_klasor = open("uzantidosyasi.uzanti", "r")
             metin = dosya_klasor.read()
             dosya_klasor.close()
-            
+    #////////////////////////
+            print "*****************",metin+chr(92)+dosya
+            if uzanti_kaydet(metin+chr(92)+dosya)==False:
+                print "girildi"
+                while uzanti_kaydet(metin+chr(92)+dosya)==False:
+                    yyy_metin=''
+                    yeni_metin=metin.split(chr(92))
+                    
+                    for al in range((len(yeni_metin)-1),0,-1):
+                        
+                        
+                        if yeni_metin[al]!='':
+                            yeni_metin[al]=''
+                            break             
+                    for i in yeni_metin:
+                        yyy_metin=yyy_metin+i+chr(92)
+
+                    metin=yyy_metin
+
             uzanti_kaydet(metin+chr(92)+dosya)
-            
-            klasor_kontrol()
-
-
-
-
-
+            klasor_kontrol(kelime)
+    else:
+        
+#///////////////////////////////////////
+        
 
 
 def baslama():
+    global adres
+
+    adres=raw_input("Aranacak adres giriniz:  ")
     
     uzanti_kaydet(adres)
+    kelime=raw_input("Aranacak kelime:")
+    if len(kelime)<2:
+        print("2 karakter veya daha fazla giriniz!!!!")
+        baslama()
     
-    
-    klasor_kontrol()
+    klasor_kontrol(kelime)
+
+
 
 baslama()
     
